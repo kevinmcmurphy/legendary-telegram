0f88f602c53fb01c413d303991e86953e672f513
Kevin McMurphy <kevin@klmc.co>
2022-03-01 09:58:58 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "UA-37804599-1"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
